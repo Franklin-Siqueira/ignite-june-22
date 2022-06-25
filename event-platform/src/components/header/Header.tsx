6c3904bcb292acb3d230823569ca5340e5c25d0a
Franklin-Siqueira <franklin.cs@outlook.com>
2022-06-25 13:40:24 -0300

    CSS adjustments

--- event-platform/src/components/header/Header.tsx
+++ event-platform/src/components/header/Header.tsx
@@ -2,12 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Logo } from '../logo/Logo'
+import logoIgnite from '../../assets/img/pages/home/Logo-igniteLab.png'
+import logoRocket from '../../assets/img/pages/home/Logo-rockeat.png'
 import './../styles/header.css'
 
 export const Header = () => {
+  const logoRocketAlt = 'Franklin Siqueira | RocketSeat | Ignite Lab | RocketSeat Logo'
+  const logoIgniteAlt = 'Franklin Siqueira | RocketSeat | Ignite Lab | Ignite Lab Logo'
   return (
     <>
-      <header className="header">
+      {/* <header className="header">
         <div className="header-left">
           <Logo />
           <p className="header-text-left">Franklin Siqueira | 2022</p>
@@ -18,7 +22,21 @@ export const Header = () => {
         <div className="header-right">
           <p className="header-text-right">RocketSeat</p>
         </div>
-      </header>
+      </header> */}
+
+      <div className="main-container-header">
+        <div className="main-container-header-marca">
+          <a href="https://github.com/franklin-siqueira">
+            <Logo />
+            <p>Franklin Siqueira | 2022</p>
+          </a>
+        </div>
+        <img className="main-container-header-image" src={logoIgnite} alt={logoRocketAlt} />
+        <a href="https://www.rocketseat.com.br/">
+          <img className="main-container-header-image" src={logoRocket} alt={logoIgniteAlt} />
+        </a>
+      </div>
+
     </>
   )
 }
--- event-platform/src/pages/Home.tsx
+++ event-platform/src/pages/Home.tsx
@@ -39,8 +39,8 @@ export const Home = () => {
   const logoRocketAlt = 'Franklin Siqueira | RocketSeat | Ignite Lab | RocketSeat Logo'
   const logoIgniteAlt = 'Franklin Siqueira | RocketSeat | Ignite Lab | Ignite Lab Logo'
   //
-  const [ name, setName ] = React.useState('')
-  const [ email, setEmail ] = React.useState('')
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
   //
   const navigateTo = useNavigate()
   //
@@ -50,7 +50,7 @@ export const Home = () => {
   //
   //    With CodeGen
   //
-  const [ newSubscription, { loading } ] = useNewSubscriptionMutation()
+  const [newSubscription, { loading }] = useNewSubscriptionMutation()
   //
   const handleSubscribeForm = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -68,38 +68,42 @@ export const Home = () => {
       <div className="main-home-container">
         <div className="main-container-header">
           <div className="main-container-header-marca">
-            <Logo />
-            <p>Franklin Siqueira | 2022</p>
+            <a href="https://github.com/franklin-siqueira">
+              <Logo />
+              <p>Franklin Siqueira | 2022</p>
+            </a>
           </div>
-          <img className="main-container-header-image" src={logoIgnite} alt={logoRocketAlt} />
-          <img className="main-container-header-image" src={logoRocket} alt={logoIgniteAlt} />
+          <img className="main-container-header-image" src={logoIgnite} alt={logoIgniteAlt} />
+          <a href="https://rocketseat.com.br">
+            <img className="main-container-header-image" src={logoRocket} alt={logoRocketAlt} />
+          </a>
         </div>
         <main className="main">
           <div className="main-hero">
             {/* <h1>Home Subscribe LandPage</h1> */}
             <div className="main-hero-left">
-              <img className="main-container-header-image" src={logoIgnite} alt={logoRocketAlt} />
+              <img className="main-container-header-image" src={logoIgnite} alt={logoIgniteAlt} />
               <h1>
                 Subscribe <span>Landing Page</span> at Ignite Lab | <span>ReactJs</span>
               </h1>
               <p>
-                Improving ReactJs skills and learning something new. That's what it's all about!
+                Improving ReactJs skills and learning something new have never been easier. At RocketSeat Ignite Lab, your path to higher employment opportunities is just around the corner. That's what it's all about!
               </p>
             </div>
             <div className="main-hero-right">
               <h1><span>Free</span> Subscription</h1>
               <form onSubmit={handleSubscribeForm}>
-                <input 
-                  type="text" 
+                <input
+                  type="text"
                   placeholder="Your name..."
-                  onChange={ e => setName(e.target.value) }
-                  />
-                <input 
-                  type="email" 
-                  placeholder="And your e-mail."
-                  onChange={ e => setEmail(e.target.value) }
+                  onChange={e => setName(e.target.value)}
                 />
-                <button type="submit" disabled={ loading }>
+                <input
+                  type="email"
+                  placeholder="And your e-mail."
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <button type="submit" disabled={loading}>
                   Get Mine Now!
                 </button>
               </form>
